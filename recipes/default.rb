aa008c84a148ff10a6d9f3f51d050e90030d7b93
Kyle Evans <evans.584@osu.edu>
2018-05-13 21:53:59 -0400

    secure config

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,19 @@
 # Copyright Holder Email:: kylebe@gmail.com
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'openssh-server'
+package 'openssh-client'
+
+template '/etc/ssh/sshd_config' do
+  source 'sshd_config.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+    notifies :restart, 'service[sshd]', :immediately
+end
+
+service 'sshd' do
+  supports :restart => true, :reload => true, :stop => true, :status => true, :start =>true
+  action [:enable, :start]
+end
